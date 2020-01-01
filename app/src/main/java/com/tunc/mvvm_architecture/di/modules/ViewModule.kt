6a4cf36098c316e4cd4dde955a561a97ac97c092
Kemal <kemal.tunc@basefy.com>
2020-01-01 19:57:48 +0300

    Change view model di

---
+++ app/src/main/java/com/tunc/mvvm_architecture/di/modules/ViewModule.kt
@@ -0,0 +1,22 @@
+package com.tunc.mvvm_architecture.di.modules
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.tunc.mvvm_architecture.base.BaseInterfaces
+import com.tunc.mvvm_architecture.di.ViewModelFactory
+import com.tunc.mvvm_architecture.di.ViewModelKey
+import com.tunc.mvvm_architecture.presentation.ui.main.MainScreenActivityViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+@Module
+abstract class ViewModule {
+    @Binds
+    internal abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MainScreenActivityViewModel::class)
+    internal abstract fun mainScreenViewModel(viewModel: MainScreenActivityViewModel<BaseInterfaces>): ViewModel
+}
